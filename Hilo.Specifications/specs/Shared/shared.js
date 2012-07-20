a60a25042ff03e14f094ced231e4451ea55cb195
Derick Bailey <derickbailey@gmail.com>
2012-07-20 12:34:20 -0500

    cleaned up function names, and moved the file check function in to the shared helpers

--- Hilo.Specifications/specs/Shared/shared.js
+++ Hilo.Specifications/specs/Shared/shared.js
@@ -1,6 +1,14 @@
 ﻿(function () {
     "use strict";
 
+    // Imports And Constants
+    // ---------------------
+
+    var thumbnailFolderName = 'tile-thumbnails';
+
+    // Private Methods
+    // ---------------
+
     var getImages = function () {
         var path = Windows.ApplicationModel.Package.current.installedLocation.path + '\\images';
         var whenFolder = Windows.Storage.StorageFolder.getFolderFromPathAsync(path);
@@ -16,8 +24,27 @@
         return paths.map(function (path) { return path.name });;
     };
 
+    var thumbnailFileExists = function (fileName) {
+        var fullPath = thumbnailFolderName + '\\' + fileName;
+        var fileOpen = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(fullPath);
+
+        var promise = new WinJS.Promise(function (complete) {
+            fileOpen.done(function () {
+                complete(true);
+            }, function () {
+                complete(false);
+            });;
+        });
+
+        return promise;
+    }
+
+    // Public API
+    // ----------
+
     WinJS.Namespace.define("Shared", {
         getImages: getImages,
-        getFileNames: getFileNames
+        getFileNames: getFileNames,
+        thumbnailFileExists: thumbnailFileExists
     });
 })();
--- Hilo.Specifications/specs/Tiles/buildThumbnails.spec.js
+++ Hilo.Specifications/specs/Tiles/buildThumbnails.spec.js
@@ -1,24 +1,8 @@
 ﻿describe('live tile', function () {
 
     describe('when updating the tile', function () {
-        var thumbnailFolderName = 'tile-thumbnails';
 
         var async = new AsyncSpec();
-
-        var fileExist = function (fileName) {
-            var fullPath = thumbnailFolderName + '\\' + fileName;
-            var fileOpen = Windows.Storage.ApplicationData.current.localFolder.getFileAsync(fullPath);
-
-            var promise = new WinJS.Promise(function(complete){
-                fileOpen.done(function () {
-                    complete(true);
-                }, function () {
-                    complete(false);
-                });;
-            });
-
-            return promise;
-        }
 
         async.beforeEach(function (complete) {
             Shared.getImages()
@@ -30,7 +14,8 @@
         async.it('should write the thumbnails to the tile thumbnails folder', function (storage) {
             storage.fileNames.forEach(function (file) {
 
-                async.await(fileExist(file)).then(function (fileExists) {
+                var thumbnailExists = Shared.thumbnailFileExists(file);
+                async.await(thumbnailExists).then(function (fileExists) {
                     expect(fileExists).toBe(true);
                 });
 
