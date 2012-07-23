a879ba0bde93cb446d7401ba95f3bf510d81c3e6
Christopher Bennage <christopher@bennage.com>
2012-07-23 09:47:25 -0700

    image repository spike

---
+++ Hilo.Specifications/specs/Hilo/imageRepository.spec.js
@@ -0,0 +1,25 @@
+﻿describe('image loader', function () {
+
+    var async = new AsyncSpec();
+    var storage = Windows.Storage;
+
+    async.beforeEach(function (complete) {
+
+        //var folderToSearch = Windows.ApplicationModel.Package.current.installedLocation.path + '\\sampleImages';
+        //var whenFolder = storage.StorageFolder.getFolderFromPathAsync(folderToSearch);
+
+        //whenFolder.then(function (folder) {
+        var folder = Windows.Storage.ApplicationData.current.temporaryFolder;
+        //var folder = Windows.Storage.KnownFolders.picturesLibrary;
+            var repo = new Hilo.ImageRepository(folder);
+            return repo.getImages(15)
+                .then(async.storage.store('images'))
+                .then(complete);
+        //});
+    });
+
+    async.it('get 15 images', function (storage) {
+        expect(storage.images.length).toBe(15);
+    });
+
+});
---
+++ Hilo/Hilo/imageRepository.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    var storage = Windows.Storage,
+        thumbnailMode = storage.FileProperties.ThumbnailMode.singleItem,
+        orderByDate = storage.Search.CommonFileQuery.orderByDate;
+
+    WinJS.Namespace.define('Hilo', {
+        ImageRepository: WinJS.Class.define(function (folder) {
+            this.folder = folder;
+        },
+        {
+            getImages: function (count) {
+                var folder = this.folder;
+                //var queryOptions = new storage.Search.QueryOptions(orderByDate, ['.jpg']);
+                var queryOptions = new storage.Search.QueryOptions();
+                var wat = folder.areQueryOptionsSupported(queryOptions);
+                var wut = folder.isCommonFileQuerySupported(orderByDate);
+                var queryResult = folder.createFileQueryWithOptions(queryOptions);
+
+                var factory = new storage.BulkAccess.FileInformationFactory(queryResult, thumbnailMode);
+
+                return factory.getFilesAsync(0, count).done(null, function () { debugger; });
+            }
+        })
+    });
+
+})();
