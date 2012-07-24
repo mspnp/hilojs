ea60a8d10dcdb88eed9b6bceaa51b1508d26eb65
Derick Bailey <derickbailey@gmail.com>
2012-07-24 10:10:53 -0700

    extracted jasmine runner code in to it's own file, and wrapped the call to run jasmine after copying the images we need to the indexed folder

---
+++ Hilo.Specifications/default.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    // # Bootstrapper
+    // This script is responsible for bootstrapping the application.
+
+    function runJasmine() {
+        var jasmineEnv = jasmine.getEnv();
+        jasmineEnv.updateInterval = 1000;
+
+        var htmlReporter = new jasmine.HtmlReporter();
+
+        jasmineEnv.addReporter(htmlReporter);
+
+        jasmineEnv.specFilter = function (spec) {
+            return htmlReporter.specFilter(spec);
+        };
+
+        jasmineEnv.execute();
+    }
+
+    var activation = Windows.ApplicationModel.Activation,
+        app = WinJS.Application,
+        nav = WinJS.Navigation;
+
+    app.addEventListener('activated', function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            args.setPromise(WinJS.UI.processAll().then(function () {
+
+                Shared.copyImagesToIndexedFolder()
+                    .then(runJasmine);
+
+            }));
+        }
+    }, false);
+
+    app.start();
+})();
