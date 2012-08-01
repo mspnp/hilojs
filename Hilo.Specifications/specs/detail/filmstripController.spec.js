22af62d3c8fa8c30e06347932db1c1190765b7ef
Derick Bailey <derickbailey@gmail.com>
2012-08-01 10:49:08 -0500

    first passing spec for a filmstrip controller, to invoke an image

---
+++ Hilo.Specifications/specs/detail/filmstripController.spec.js
@@ -0,0 +1,50 @@
+﻿describe("film strip controller", function () {
+
+    var filmstripController, el;
+
+    beforeEach(function(){
+        el = {
+            winControl: {
+                selection: {
+                    getIndices: function () {
+                        return [0];
+                    },
+                },
+                handlers: {},
+                addEventListener: function(type, handler){
+                    this.handlers[type] = handler;
+                },
+                dispatchEvent: function (type, args) {
+                    if (!this.handlers[type]) {
+                        throw new Error("Handler for '" + type + "' not found.")
+                    }
+                    this.handlers[type](args);
+                }
+            }
+        };
+
+        filmstripController = new Hilo.Detail.FilmstripController(el);
+    });
+
+    describe("when invoking a thumbnail", function () {
+        var handler = function (args) {
+            handler.wasCalled = true;
+            handler.args = args.detail;
+        }
+
+        beforeEach(function () {
+            filmstripController.addEventListener("imageInvoked", handler);
+
+            el.winControl.dispatchEvent("iteminvoked", {});
+        });
+
+        it("should trigger an imageInvoked event", function () {
+            expect(handler.wasCalled).equals(true);
+        });
+
+        it("should tell me the index of the clicked item", function () {
+            expect(handler.args.itemIndex).equals(0);
+        });
+    });
+
+});
---
+++ Hilo/Hilo/detail/filmstripController.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+
+    // Imports And Constants
+    // ---------------------
+
+    // Private Methods
+    // ---------------
+
+    function FilmstripController(el) {
+        this.el = el;
+        this.winControl = el.winControl;
+        this.setupControlHandlers();
+    }
+
+    var filmstripController = {
+        setupControlHandlers: function () {
+            this.winControl.addEventListener("iteminvoked", this.itemClicked.bind(this));
+        },
+
+        getSelectedIndices: function(){
+            return this.winControl.selection.getIndices();
+        },
+
+        itemClicked: function () {
+            var indices = this.getSelectedIndices();
+            this.dispatchEvent("imageInvoked", {
+                itemIndex: indices[0]
+            });
+        }
+    }
+
+    // Public API
+    // ----------
+
+    WinJS.Namespace.define("Hilo.Detail", {
+        FilmstripController: WinJS.Class.mix(FilmstripController, filmstripController, WinJS.Utilities.eventMixin)
+    });
+
+})();
