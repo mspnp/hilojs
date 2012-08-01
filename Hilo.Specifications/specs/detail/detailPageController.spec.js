0d0424bdacc82196129e7e2d3af787caeae97d10
Derick Bailey <derickbailey@gmail.com>
2012-08-01 13:35:08 -0500

    first spec for detal page controller, to mediate between flip view and filmstrip when clicking something in the filmstrip

---
+++ Hilo.Specifications/specs/detail/detailPageController.spec.js
@@ -0,0 +1,32 @@
+﻿describe("Detail page", function () {
+    describe("detail page controller", function () {
+
+        var detailPageController, filmstripController, flipviewController;
+
+        beforeEach(function () {
+            var FilmStrip = WinJS.Class.mix(function () { }, WinJS.Utilities.eventMixin);
+            filmstripController = new FilmStrip();
+            
+            flipviewController = {
+                showImageAt: function (index) {
+                    flipviewController.showImageAt.wasCalled = true;
+                    flipviewController.showImageAt.itemIndex = index;
+                }
+            };
+
+            detailPageController = new Hilo.Detail.DetailPageController(flipviewController, filmstripController);
+        });
+
+        describe("when an image has been activated", function () {
+            beforeEach(function () {
+                filmstripController.dispatchEvent("imageInvoked", { itemIndex: 1 });
+            });
+
+            it("should show the image", function () {
+                expect(flipviewController.showImageAt.wasCalled).equals(true);
+                expect(flipviewController.showImageAt.itemIndex).equals(1);
+            });
+        });
+
+    });
+});
---
+++ Hilo/Hilo/detail/detailPageController.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    // Private Methods
+    // ---------------
+
+    function DetailPageController(flipview, filmstrip) {
+        this.flipview = flipview;
+        this.filmstrip = filmstrip;
+        filmstrip.addEventListener("imageInvoked", this.imageClicked.bind(this));
+    }
+
+    var detailPageController = {
+        imageClicked: function (args) {
+            this.flipview.showImageAt(args.detail.itemIndex);
+        }
+    };
+
+    // Public API
+    // ----------
+
+    WinJS.Namespace.define("Hilo.Detail", {
+        DetailPageController: WinJS.Class.mix(DetailPageController, detailPageController, WinJS.Utilities.eventMixin)
+    });
+})();
