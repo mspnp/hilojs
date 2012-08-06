ebdb7c5a873f5d6ad690f9d0192ba9aa18fdd248
Derick Bailey <derickbailey@gmail.com>
2012-08-06 12:32:26 -0500

    first pass at a query object for images, that supports setting a count

---
+++ Hilo.Specifications/specs/queries/imageQueryBuilder.spec.js
@@ -0,0 +1,41 @@
+﻿describe("image query builder", function () {
+
+    var queryBuilder;
+
+    beforeEach(function(done){
+        var whenFolderIsReady = Windows.Storage.ApplicationData.current.localFolder.getFolderAsync("Indexed");
+
+        whenFolderIsReady.then(function (folder) {
+            queryBuilder = new Hilo.ImageQueryBuilder(folder);
+            done();
+        });
+    });
+
+    describe("when building a query", function () {
+        var query;
+
+        beforeEach(function(){
+            query = queryBuilder.build();
+        });
+
+        it("should return a query object that can be executed", function () {
+            expect(query.execute).to.be.a('function');
+        });
+    });
+
+    describe("when executing a query that specifies the number of images to load", function () {
+        var query;
+
+        beforeEach(function () {
+            query = queryBuilder.count(1).build();
+        });
+
+        it("should load the specified number of images", function (done) {
+            query.execute().then(function (images) {
+                expect(images.length).equals(1);
+                done();
+            });
+        });
+    });
+
+});
---
+++ Hilo/Hilo/imageQueryBuilder.js
@@ -0,0 +1,79 @@
+﻿(function () {
+    'use strict';
+
+    // Imports And Constants
+    var storage = Windows.Storage,
+        promise = WinJS.Promise,
+        knownFolders = Windows.Storage.KnownFolders,
+        search = Windows.Storage.Search,
+        commonFileQuery = storage.Search.CommonFileQuery;
+
+    // Private Methods
+    // ---------------
+
+    function ImageQueryBuilder(folder) {
+        this._settings = {};
+        this._set('folder', folder);
+        this._set('fileTypes', ['.jpg', '.png', '.bmp', '.tiff', '.gif']);
+        this._set('thumbnailMode', storage.FileProperties.ThumbnailMode.singleItem);
+        this._set('thumbnailSize', 1024);
+        this._set('sortOrder', commonFileQuery.orderByDate);
+        this._set('indexerOption', search.IndexerOption.useIndexerWhenAvailable);
+        this._set('count', 15);
+    }
+
+    var imageQueryBuilderMethods = {
+        
+        build: function () {
+            return new Query(this._settings);
+        },
+
+        count: function(count){
+            return this._set('count', count);
+        },
+
+        _set: function(key, value){
+            this._settings[key] = value;
+            return this;
+        },
+
+    };
+
+    // Query Object
+    // ------------
+
+    function QueryObject(settings) {
+        var dupSettings = Object.create(settings);
+        this.settings = Object.freeze(dupSettings);
+
+        this.queryOptions = this._buildQueryOptions();
+        this.fileQuery = this._buildFileQuery();
+    }
+
+    var queryObjectMethods = {
+        execute: function () {
+            return this.fileQuery.getFilesAsync(0, this.settings.count);
+        },
+
+        _buildQueryOptions: function () {
+            var queryOptions = new storage.Search.QueryOptions(this.settings.sortOrder, this.settings.fileTypes);
+            queryOptions.indexerOption = this.settings.indexerOption;
+
+            return queryOptions;
+        },
+
+        _buildFileQuery: function () {
+            return this.settings.folder.createFileQueryWithOptions(this.queryOptions);
+        }
+    };
+
+    var Query = WinJS.Class.define(QueryObject, queryObjectMethods);
+
+    // Public API
+    // ----------
+
+    WinJS.Namespace.define("Hilo", {
+        ImageQueryBuilder: WinJS.Class.define(ImageQueryBuilder, imageQueryBuilderMethods)
+    });
+
+})();
