48a62045500dc98e4e2fa44fe1385f600e24a946
Derick Bailey <derickbailey@gmail.com>
2012-08-06 14:13:47 -0500

    giving in to the dark side

--- Hilo.Specifications/specs/queries/imageQueryBuilder.spec.js
+++ Hilo.Specifications/specs/queries/imageQueryBuilder.spec.js
@@ -1,9 +1,9 @@
-﻿describe("image query builder", function () {
+﻿describe('image query builder', function () {
 
     var queryBuilder;
 
     beforeEach(function (done) {
-        var whenFolderIsReady = Windows.Storage.ApplicationData.current.localFolder.getFolderAsync("Indexed");
+        var whenFolderIsReady = Windows.Storage.ApplicationData.current.localFolder.getFolderAsync('Indexed');
 
         whenFolderIsReady.then(function (folder) {
             queryBuilder = new Hilo.ImageQueryBuilder(folder);
@@ -11,27 +11,27 @@
         });
     });
 
-    describe("when building a query", function () {
+    describe('when building a query', function () {
         var query;
 
         beforeEach(function () {
             query = queryBuilder.build();
         });
 
-        it("should return a query object that can be executed", function () {
+        it('should return a query object that can be executed', function () {
             expect(query.execute).to.be.a('function');
         });
     });
 
 
-    describe("when executing a query that specifies the number of images to load", function () {
+    describe('when executing a query that specifies the number of images to load', function () {
         var query;
 
         beforeEach(function () {
             query = queryBuilder.count(1).build();
         });
 
-        it("should load the specified number of images", function (done) {
+        it('should load the specified number of images', function (done) {
             query.execute().then(function (images) {
                 expect(images.length).equals(1);
                 done();
@@ -39,14 +39,14 @@
         });
     });
 
-    describe("when executing a query that does not specifies the number of images to load", function () {
+    describe('when executing a query that does not specifies the number of images to load', function () {
         var query;
 
         beforeEach(function () {
             query = queryBuilder.build();
         });
 
-        it("should load all images in the folder", function (done) {
+        it('should load all images in the folder', function (done) {
             query.execute().then(function (images) {
                 expect(images.length).equals(17);
                 done();
@@ -54,7 +54,7 @@
         });
     });
 
-    describe("when serializing and then deserializing a query object", function () {
+    describe('when serializing and then deserializing a query object', function () {
         var deserializedQuery, serializedQuery;
 
         beforeEach(function () {
@@ -64,21 +64,22 @@
             deserializedQuery = Hilo.ImageQueryBuilder.deserialize(serializedQuery);
         });
 
-        it("should restore all of the options for the query", function () {
+        it('should restore all of the options for the query', function () {
             expect(deserializedQuery.settings).deep.equals(serializedQuery);
         });
     });
 
-    describe("when specifying a month and year for images", function () {
+    describe('when specifying a month and year for images', function () {
         var query;
 
         beforeEach(function () {
-            query = queryBuilder.forMonthAndYear("Jan 2012").build();
+            query = queryBuilder.forMonthAndYear('Jan 2012').build();
         });
 
-        it("should configure the query for the specified month and year", function () {
+        it('should configure the query for the specified month and year', function () {
             expect(query.queryOptions.applicationSearchFilter).equals('taken: Jan 2012');
         });
     });
 
+ 
 });
