7e4aa93105f43d77610925a67a8ad3bb4ddb20e6
Derick Bailey <derickbailey@gmail.com>
2012-08-07 10:14:41 -0500

    fixed the hub page spec for checking url

--- Hilo.Specifications/specs/hub/hub.spec.js
+++ Hilo.Specifications/specs/hub/hub.spec.js
@@ -4,36 +4,16 @@
     var promise = WinJS.Promise;
     var hubPageUrl = '/Hilo/hub/hub.html';
 
-    function mockRepository() {
-        return {
-            getBindableImages: function () {
-                return promise.wrap([{
-                    name: 'some-file.jpg',
-                    url: 'url("some-url")',
-                    className: 'thumbnail'
-                }]);
-            }
-        }
-    };
-
     beforeEach(function (done) {
-        this.originalRepository = Hilo.ImageRepository;
-        Hilo.ImageRepository = mockRepository;
-
         WinJS.Navigation.navigate(hubPageUrl);
-
         setTimeout(done, 500);
-    });
-
-    afterEach(function () {
-        Hilo.ImageRepository = this.originalRepository;
     });
 
     describe('when activated', function () {
 
         it('should display images from the picture library', function () {
             var item = document.querySelector('.thumbnail');
-            expect(item.style.backgroundImage).equal('url("some-url")');
+            expect(item.style.backgroundImage).match(/url\("blob:.*"\)/);
         });
     });
 
