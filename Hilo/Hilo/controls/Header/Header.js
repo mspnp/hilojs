b397a1e9765dd52502db6a3e85dbe1f50a0e9c88
Derick Bailey <derickbailey@gmail.com>
2012-08-08 14:45:47 -0500

    commented the header file

--- Hilo/Hilo/controls/Header/Header.js
+++ Hilo/Hilo/controls/Header/Header.js
@@ -3,29 +3,38 @@
 (function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/Hilo/controls/Header/Header.html", {
-        // This function is called whenever a user navigates to this page. It
-        // populates the page elements with the app's data.
-        ready: function (element, options) {
+    // This control provides a common implementation for the header of a page,
+    // including the page title, and back button. The back button is facilitated
+    // by the `navigation.js` file, while the page title is set by passing a
+    // `titleResource` option to the page:
+    // 
+    // ```html
+    // <div data-win-control="WinJS.UI.HtmlControl" 
+    //       data-win-options="{uri: '/Hilo/Controls/Header/Header.html, titleResource: 'someResource'}"
+    //  ></div>
+    // ```
+    //
+    // The `titleResource` is used to look up the resource string in the application's
+    // localization resources. The name that is provided for this parameter is the key 
+    // that is used to look up the resource. 
 
+    WinJS.UI.Pages.define("/Hilo/controls/Header/Header.html", {
+
+        ready: function (element, options) {
+            var pageTitle, title;
+
+            // If the `titleResource` was provided, set the page title
             if (options.titleResource) {
-                var pageTitle = element.querySelector("#pageTitle");
-                var title = WinJS.Resources.getString(options.titleResource);
+
+                // find the `#pageTitle` HTML element
+                pageTitle = element.querySelector("#pageTitle");
+
+                // Load and set the localized resource for the page title
+                title = WinJS.Resources.getString(options.titleResource);
                 pageTitle.textContent = title.value;
             }
 
-        },
-
-        updateLayout: function (element, viewState, lastViewState) {
-            /// <param name="element" domElement="true" />
-            /// <param name="viewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
-            /// <param name="lastViewState" value="Windows.UI.ViewManagement.ApplicationViewState" />
-
-            // TODO: Respond to changes in viewState.
-        },
-
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
         }
+
     });
 })();
