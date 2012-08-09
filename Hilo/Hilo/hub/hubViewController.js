9c77d4388a38e6684dee222ca9d1dbeb4aedcbf0
Christopher Bennage <christopher@bennage.com>
2012-08-09 11:27:00 -0700

    comments the Hub page controller and related list view controller

--- Hilo/Hilo/hub/hubViewController.js
+++ Hilo/Hilo/hub/hubViewController.js
@@ -1,9 +1,29 @@
 ﻿(function () {
     'use strict';
 
-    // Private Methods
-    // ---------------
+    // Hub View Controller Constructor
+    // -------------------------------
 
+    // The Hub view controller is an implementation of [the mediator pattern][1],
+    // designed to coordinate multiple components of an individual page to
+    // facilitate all of the functionality of that page. 
+    //
+    // The HubViewController requires 4 parameters for the constructor function:
+    //
+    // 1. `nav` - the `WinJS.Navigation` object, used to navigate to other pages
+    // 2. `imageNav` - an instance of `Hilo.Controls.ImageNav.ImageNavController`
+    // 3. `listView` - an instance of `Hilo.Hub.ListViewController`
+    // 4. `queryBuilder` - an instance of `Hilo.ImageQueryBuilder`
+    // 
+    // Each individual component of the page is focused on one specific set 
+    // of behaviors - both visually and in code. This creates a very clean
+    // separation of concerns for each functional area of the screen. The 
+    // mediator, then, brings all of the functionality of each component 
+    // together. It listens to events from one component and determines what 
+    // to do with the other components in response. 
+    //
+    // [1]: http://en.wikipedia.org/wiki/Mediator_pattern
+    //
     function HubViewController(nav, imageNav, listview, queryBuilder) {
         this.nav = nav;
         this.imageNav = imageNav;
@@ -11,32 +31,69 @@
         this.queryBuilder = queryBuilder
     };
 
-    var hubViewMethods = {
-        start: function(){
+    // Hub View Controller Methods
+    // ---------------------------
+
+    var hubViewControllerMethods = {
+
+        // Starts processing the events from individual components, to 
+        // facilitate the functionality of the other components.
+        start: function () {
+
+            // The [ECMASCript5 `bind`][2] function is used to ensure that the
+            // context (the `this` variable) of each of the specified
+            // callback functions is set correctly, when the event triggers
+            // and the callback is executed.
+            // 
+            // [2]: http://msdn.microsoft.com/en-us/library/windows/apps/ff841995
+            //
             this.listview.addEventListener('selectionChanged', this.selectionChanged.bind(this));
             this.listview.addEventListener('itemInvoked', this.itemClicked.bind(this));
         },
 
+        // The callback method for item selection in the listview changing.
+        // This function coordinates the selection changes with the 
+        // ImageNavController to show and hide it appropriately.
         selectionChanged: function (args) {
+
             if (args.detail.hasItemSelected) {
+                // If someone an image is selected, show the image nav
+                // app bar with the "crop" and "rotate" buttons
                 this.imageNav.setImageIndex(args.detail.itemIndex);
             } else {
+                // If no images are selected, hide the app bar
                 this.imageNav.clearImageIndex();
             }
+
         },
 
+        // When an item is "invoked" (clicked or tapped), navigate to
+        // the detail screen to display this image in the month-group
+        // that it belongs to, based on the "ItemDate" of the picture.
         itemClicked: function (args) {
-            var monthNames = ['January','February','March','April','May','June', 'July', 'August','September','October','November','December'];
+
+            // TODO: find a better way to handle converting month numbers in to month names
+            var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+            // get the `Hilo.Picture` item that was bound to the invoked image
             var picture = args.detail.item.data;
+
+            // Build various parameters to determine the image
+            // index and the month and year it was taken.
+            // TODO: get the actual item index based on the query that is built
             var itemIndex = picture.groupIndex;
             var dateTaken = picture.itemDate;
             var monthAndYear = monthNames[dateTaken.getMonth()] + ' ' + dateTaken.getFullYear();
 
+            // Build a query to get all images from the month and year of this image
             this.queryBuilder
                 .bindable()
                 .forMonthAndYear(monthAndYear);
 
             var query = this.queryBuilder.build();
+
+            // Navigate to the detail view, specifying the month query to
+            // show, and the index of the individual item that was invoked
             this.nav.navigate('/Hilo/detail/detail.html', { itemIndex: itemIndex, query: query });
         }
     };
@@ -45,7 +102,7 @@
     // ----------
 
     WinJS.Namespace.define('Hilo.Hub', {
-        HubViewController: WinJS.Class.define(HubViewController, hubViewMethods)
+        HubViewController: WinJS.Class.define(HubViewController, hubViewControllerMethods)
     });
 
 })();
--- Hilo/Hilo/hub/listViewController.js
+++ Hilo/Hilo/hub/listViewController.js
@@ -15,9 +15,27 @@
         cellHeight: 200
     };
 
-    // Private Methods
-    // ---------------
+    // List View Controller Constructor
+    // --------------------------------
 
+    // The list view controller manages the contents and interactions of
+    // the Hub page's list view. It listens to events from the 
+    // [WinJS.UI.ListView][1] control and determines what to do based on
+    // the specific event. 
+    //
+    // The ListViewController takes 2 parameters:
+    //
+    // 1. `el` - the DOM element for the `WinJS.UI.ListView` to control
+    // 2. `appView` - a reference to [Windows.UI.ViewManagement.ApplicationView][2]
+    //
+    // This object requires a collaborator that can listen to the events that 
+    // it triggers. The collaborator (in this case, the `Hilo.Hub.HubViewController`)
+    // determines how other parts of the page will behave based on the events raised 
+    // by this controller.
+    //
+    // [1]: http://msdn.microsoft.com/en-us/library/windows/apps/br211837.aspx
+    // [2]: http://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.viewmanagement.applicationview
+    //
     function ListViewController(el, appView) {
         this.el = el;
         this.lv = el.winControl;
@@ -25,23 +43,63 @@
         this.setup();
     }
 
-    var controllerMethods = {
+    // List View Controller Methods
+    // ----------------------------
 
+    var listViewControllerMethods = {
+
+        // Set up the default layout for the view, and set up the
+        // event handlers from the ListView control.
         setup: function () {
             this.lv.layout = this.selectLayout(this.appView.value);
 
+            // The [ECMASCript5 `bind`][3] function is used to ensure that the
+            // context (the `this` variable) of the callback functions is set correctly.
+            // Otherwise, the callback functions would not resolve `this` correctly 
+            // when the events are raised and the callbacks are executed.
+            // 
+            // [3]: http://msdn.microsoft.com/en-us/library/windows/apps/ff841995
+            //
             this.lv.addEventListener('iteminvoked', this.imageNavigated.bind(this));
             this.lv.addEventListener('selectionchanged', this.imageSelected.bind(this));
         },
 
+        // Event handler for selecting an image
+        imageSelected: function (args) {
+            var itemIndex = this.getIndices();
+            var hasItemSelected = itemIndex.length > 0;
+            this.dispatchEvent("selectionChanged", {hasItemSelected: hasItemSelected });
+        },
+
+        // Event handler for "invoking" (clicking or tapping) an image
+        imageNavigated: function (args) {
+            var that = this;
+            args.detail.itemPromise.then(function (item) {
+                that.dispatchEvent("itemInvoked", {
+                    item: item,
+                    itemIndex: args.detail.itemIndex
+                });
+            });
+        },
+
+        // Bind an array of objects to the `ListView` by converting
+        // it in to a bindable list, and data source.
         setDataSource: function (items) {
             this.lv.itemDataSource = new WinJS.Binding.List(items).dataSource;
         },
 
+        // Change the layout of the list view based on the application's
+        // [application view state][4]. 
+        // 
+        // [4]: http://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.viewmanagement.applicationviewstate
         setViewState: function (viewState) {
             this.lv.layout = this.selectLayout(viewState);
         },
 
+        // Determines the correct layout for the screen based
+        // on the current view state and last view state. If the
+        // current view state is the same as the last view state,
+        // no changes are made to the layout.
         selectLayout: function (viewState, lastViewState) {
 
             if (lastViewState === viewState) { return; }
@@ -57,35 +115,17 @@
             }
         },
 
+        // Get the indices of the selected items from the ListView
         getIndices: function () {
             return this.lv.selection.getIndices();
-        },
-
-        imageSelected: function (args) {
-            var itemIndex = this.getIndices();
-            var hasItemSelected = itemIndex.length > 0;
-            this.dispatchEvent("selectionChanged", {hasItemSelected: hasItemSelected });
-        },
-
-        imageNavigated: function (args) {
-            var that = this;
-            args.detail.itemPromise.then(function (item) {
-                that.dispatchEvent("itemInvoked", {
-                    item: item,
-                    itemIndex: args.detail.itemIndex
-                });
-            });
         }
     };
 
     // Public API
-    // ----------
-
-    var Klass = WinJS.Class.define(ListViewController, controllerMethods);
-    Klass = WinJS.Class.mix(Klass, WinJS.Utilities.eventMixin);
+    // ----------   
 
     WinJS.Namespace.define("Hilo.Hub", {
-        ListViewController: Klass
+        ListViewController: WinJS.Class.mix(ListViewController, listViewControllerMethods, WinJS.Utilities.eventMixin)
     });
 
 })();
