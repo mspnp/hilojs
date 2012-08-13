91e328c3b20b40eb5d98ce89b536c71642ec31ad
Derick Bailey <derickbailey@gmail.com>
2012-08-13 15:47:32 -0500

    hack to get image rotation in place, visually. no file saving or modification, yet. just a visual rotate via buttons

--- Hilo/Hilo/rotate/rotate.js
+++ Hilo/Hilo/rotate/rotate.js
@@ -15,15 +15,34 @@
             var section = document.querySelector("section[role='main']");
             section.innerHtml = "";
 
-            var img = document.createElement("img");
-            section.appendChild(img);
-            img.addEventListener("load", function () {
-                ui.Animation.fadeIn(img);
-            });
+            var img = document.querySelector("#image");
 
             queryBuilder.build().execute().then(function (selected) {
                 img.src = URL.createObjectURL(selected[0]);
             });
+
+            var clock = document.querySelector("#rotateClockwise");
+            var counter = document.querySelector("#rotateCounterclockwise");
+            document.querySelector("#appbar").winControl.show();
+
+            var rotation = 0;
+
+            function rotateImage(angle) {
+                var style = "rotate(" + rotation + "deg)";
+                img.style.transform = style;
+                console.log(style);
+            }
+
+            clock.addEventListener("click", function (args) {
+                rotation += 90;
+                rotateImage(rotation);
+            });
+
+            counter.addEventListener("click", function (args) {
+                rotation -= 90;
+                rotateImage(rotation);
+            });
+
         },
 
         unload: function () {
@@ -31,6 +50,6 @@
         }
     };
 
-    WinJS.UI.Pages.define("/Hilo/rotate.html", page);
+    WinJS.UI.Pages.define("/Hilo/rotate/rotate.html", page);
 
 }());
