d23497de2a60edbe71525c12abdbc0abfe38fa09
Derick Bailey <derickbailey@gmail.com>
2012-08-14 10:45:19 -0500

    basic specs for behavior of rotate menu bar buttons

--- Hilo.Specifications/specs/Rotate/menuController.spec.js
+++ Hilo.Specifications/specs/Rotate/menuController.spec.js
@@ -1,34 +1,123 @@
 ﻿describe("rotate menu controller", function () {
+	var WinControlStub = WinJS.Class.mix(function () { this.winControl = this }, WinJS.Utilities.eventMixin);
+
+	var menuController, el, saveButton, cancelButton, clockwiseButton, counterClockwiseButton;
 
 	beforeEach(function () {
+		saveButton = new WinControlStub();
+		cancelButton = new WinControlStub();
+		clockwiseButton = new WinControlStub();
+		counterClockwiseButton = new WinControlStub();
+
+		el = {
+			querySelector: function (selector) {
+				if (selector === "#save") { return saveButton; }
+				if (selector === "#cancel") { return cancelButton; }
+				if (selector === "#clockwise") { return clockwiseButton; }
+				if (selector === "#counterClockwise") { return counterClockwiseButton; }
+			}
+		};
+
+		menuController = new Hilo.Rotate.MenuController(el);
 	});
 
 	describe("when the rotate menu is initialized", function () {
-		it("should disable the save button");
 
-		it("should disable the cancel button");
+		it("should disable the save button", function () {
+			expect(saveButton.disabled).equals(true);
+		});
+
+		it("should disable the cancel button", function () {
+			expect(cancelButton.disabled).equals(true);
+		});
 	});
 
 	describe("when clicking rotate clockwise", function () {
-		it("should trigger image rotation, 90 degrees");
+		var imageRotateHandler;
 
-		it("should enable the save button");
+		beforeEach(function(){
+			imageRotateHandler = function (args) {
+				imageRotateHandler.args = args.detail;
+			};
 
-		it("should enable the cancel button");
+			menuController.addEventListener("rotate", imageRotateHandler);
+
+			clockwiseButton.dispatchEvent("click");
+		});
+
+		it("should trigger image rotation, 90 degrees", function () {
+			expect(imageRotateHandler.args.rotateDegrees).equals(90);
+		});
+
+		it("should enable the save button", function () {
+			expect(saveButton.disabled).equals(false);
+		});
+
+		it("should enable the cancel button", function(){
+			expect(cancelButton.disabled).equals(false);
+		});
 	});
 
 	describe("when clicking rotate counter-clockwise", function () {
-		it("should trigger image rotation, -90 degrees");
+		var imageRotateHandler;
 
-		it("should enable the save button");
+		beforeEach(function(){
+			imageRotateHandler = function (args) {
+				imageRotateHandler.args = args.detail;
+			};
 
-		it("should enable the cancel button");
+			menuController.addEventListener("rotate", imageRotateHandler);
+
+			counterClockwiseButton.dispatchEvent("click");
+		});
+
+		it("should trigger image rotation, -90 degrees", function () {
+			expect(imageRotateHandler.args.rotateDegrees).equals(-90);
+		});
+
+		it("should enable the save button", function () {
+			expect(saveButton.disabled).equals(false);
+		});
+
+		it("should enable the cancel button", function(){
+			expect(cancelButton.disabled).equals(false);
+		});
 	});
 
 	describe("when clicking save", function () {
+		var saveHandler;
+
+		beforeEach(function () {
+			saveHandler = function () {
+				saveHandler.wasCalled = true;
+			}
+
+			menuController.addEventListener("save", saveHandler);
+
+			saveButton.dispatchEvent("click");
+		});
+
+		it("should trigger image save", function () {
+			expect(saveHandler.wasCalled).equals(true);
+		});
 	});
 
 	describe("when clicking cancel", function () {
+		var cancelHandler;
+
+		beforeEach(function () {
+			cancelHandler = function () {
+				cancelHandler.wasCalled = true;
+			}
+
+			menuController.addEventListener("cancel", cancelHandler);
+
+			cancelButton.dispatchEvent("click");
+		});
+
+		it("should trigger to cancel the image changes", function () {
+			expect(cancelHandler.wasCalled).equals(true);
+		});
 	});
 
 });
--- Hilo/Hilo/rotate/menuController.js
+++ Hilo/Hilo/rotate/menuController.js
@@ -1,4 +1,75 @@
 ﻿(function () {
 	"use strict";
 
+	// Menu Controller Constructor
+	// ---------------------------
+
+	function MenuController(el) {
+		this.el = el;
+		this.menu = el.winControl;
+		this.setupButtons();
+	}
+
+	// Menu Controller Methods
+	// -----------------------
+
+	var menuControllerMethods = {
+		setupButtons: function () {
+			this.clockwiseButton = this.el.querySelector("#clockwise").winControl;
+			this.clockwiseButton.addEventListener("click", this.rotateClockwise.bind(this));
+
+			this.counterClockwiseButton = this.el.querySelector("#counterClockwise").winControl;
+			this.counterClockwiseButton.addEventListener("click", this.rotateCounterClockwise.bind(this));
+
+			this.saveButton = this.el.querySelector("#save").winControl;
+			this.saveButton.addEventListener("click", this.saveChanges.bind(this));
+
+			this.cancelButton = this.el.querySelector("#cancel").winControl;
+			this.cancelButton.addEventListener("click", this.cancelChanges.bind(this));
+
+			this._disableButtons();
+		},
+
+		rotateClockwise: function (args) {
+			this._enableButtons();
+
+			this.dispatchEvent("rotate", {
+				rotateDegrees: 90
+			});
+		},
+
+		rotateCounterClockwise: function (args) {
+			this._enableButtons();
+
+			this.dispatchEvent("rotate", {
+				rotateDegrees: -90
+			});
+		},
+
+		saveChanges: function () {
+			this.dispatchEvent("save", {});
+		},
+
+		cancelChanges: function () {
+			this.dispatchEvent("cancel", {});
+		},
+
+		_enableButtons: function () {
+			this.saveButton.disabled = false;
+			this.cancelButton.disabled = false;
+		},
+
+		_disableButtons: function () {
+			this.saveButton.disabled = true;
+			this.cancelButton.disabled = true;
+		}
+	};
+
+	// Public API
+	// ----------
+
+	WinJS.Namespace.define("Hilo.Rotate", {
+		MenuController: WinJS.Class.mix(MenuController, menuControllerMethods, WinJS.Utilities.eventMixin)
+	});
+
 })();
