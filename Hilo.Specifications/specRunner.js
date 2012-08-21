ac92d68bfa6b9e2a2e20e96e54069fb552ac75b1
Derick Bailey <derickbailey@gmail.com>
2012-08-21 09:43:27 -0500

    commenting the spec runner

--- Hilo.Specifications/specRunner.js
+++ Hilo.Specifications/specRunner.js
@@ -23,11 +23,23 @@
 	// ------------------
 
 	var specRunnerMethods = {
+
+	    // We're using the [bdd syntax][1] for Mocha, and
+	    // using [Chai.js][2] for the `expect` style assertions.
+	    //
+	    // [1]: http://visionmedia.github.com/mocha/
+	    // [2]: http://chaijs.com/
+	    //
 		configureMocha: function(){
 			global.expect = chai.expect;
 			global.mocha.setup("bdd");
 		},
 
+	    // Run the specs for this system by finding all of the
+	    // needed .js files for the source code, the helper files,
+	    // and the specs. Inject a `<script>` tag in to the DOM
+	    // for each file that it finds, and then start the test
+        // runner.
 		run: function () {
 			this.appFolder = Windows.ApplicationModel.Package.current.installedLocation;
 			this.configureMocha();
@@ -39,6 +51,9 @@
 				.done(null, this.triggerError.bind(this));
 		},
 
+	    // If an error occurred, dispatch it so that the
+	    // app running the tests can handle it and show the
+        // error message.
 		triggerError: function (error) {
 			this.dispatchEvent("error", error);
 		},
@@ -47,6 +62,8 @@
 			global.mocha.run();
 		},
 
+	    // Locate the .js files for the application via
+        // the `src` folder, and inject them in to the DOM
 		injectPageControls: function () {
 			return this.getFolder(this.srcFolder)
 				.then(this.getJSFileNames.bind(this))
@@ -54,6 +71,8 @@
 				.then(this.addScriptsToBody.bind(this));
 		},
 
+	    // Locate the .js helper files from the `helpers`
+        // folder and inject them in to the DOM
 		injectHelpers: function () {
 			return this.getFolder(this.helperFolder)
 				.then(this.getJSFileNames.bind(this))
@@ -61,6 +80,9 @@
 				.then(this.addScriptsToBody.bind(this));
 		},
 
+	    // Location the *spec.js files for the specs that
+	    // test the application files, from the `specs`
+	    // folder and inject them in to the DOM
 		injectSpecList: function () {
 			return this.getFolder(this.specFolder)
 				.then(this.getSpecFileNames.bind(this))
@@ -68,6 +90,16 @@
 				.then(this.addScriptsToBody.bind(this));
 		},
 
+	    // A recursive function that returns a [StorageFolder][3]
+	    // based on a `folder/name/` parameter. 
+        //
+		// Since the built in [getFolderAsync][4] method won't allow
+	    // a `/` in the folder to load, relative paths cannot
+	    // be loaded without recursion.
+	    //
+	    // [3]: http://msdn.microsoft.com/library/windows/apps/BR227230
+	    // [4]: http://msdn.microsoft.com/en-us/library/windows/apps/windows.storage.storagefolder.getfolderasync.aspx
+	    //
 		getFolder: function (folderName, parentFolder) {
 			parentFolder = parentFolder || this.appFolder;
 
@@ -91,16 +123,24 @@
 			}
 		},
 
+	    // Use a regex to find all *.js files in the specified folder,
+        // including all sub-folders.
 		getJSFileNames: function (folder) {
 			var nameTest = /.*js$/;
 			return this._buildFileListFromRegex(nameTest, folder);
 		},
 
+	    // Use a regex to find all *spec.js files in the specified folder,
+        // including all sub-folders.
 		getSpecFileNames: function (folder) {
 			var specTest = /[Ss][Pp][Ee][Cc].*js/;
 			return this._buildFileListFromRegex(specTest, folder);
 		},
 
+	    // Build a `<script>` tag array from an array of [StorageFile][5]
+	    // objects.
+	    //
+	    // [5]: http://msdn.microsoft.com/en-us/library/windows/apps/windows.storage.storagefile.aspx
 		buildScriptTags: function (fileList) {
 			var appPath = this.appFolder.path;
 
@@ -115,6 +155,8 @@
 			return WinJS.Promise.as(specList);
 		},
 
+	    // Inject the `<script>` tag array, an array of DOM elements,
+	    // in to the DOM, at the bottom of the `<body>` tag.
 		addScriptsToBody: function (scriptTags) {
 			var body = document.querySelector("body");
 			scriptTags.forEach(function (tag) {
@@ -124,6 +166,10 @@
 			return WinJS.Promise.as(true);
 		},
 
+	    // Build a list of files from a folder, using the supplied
+	    // regex to determine if the file should be included in the
+	    // resulting list. Files are loaded from nested sub-folders,
+        // ordered by file name.
 		_buildFileListFromRegex: function(regEx, folder){
 			var fileQuery = folder.getFilesAsync(Windows.Storage.Search.CommonFileQuery.orderByName);
 
