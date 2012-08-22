f705be414516562971cbd45d4910d94074a59761
Christopher Bennage <christopher@bennage.com>
2012-08-22 14:21:57 -0700

    comments on the date formatter

--- Hilo/Hilo/dateFormatter.js
+++ Hilo/Hilo/dateFormatter.js
@@ -10,18 +10,32 @@
 ﻿(function () {
     "use strict";
 
+    // This module makes use of the WinRT API
+    // ` Windows.Globalization.DateTimeFormatting.DateTimeFormatter`
+    //
+    // See the official documentation here:
+    // http://msdn.microsoft.com/en-us/library/windows/apps/windows.globalization.datetimeformatting.datetimeformatter.aspx
+
     // Imports And Constants
     // ---------------------
     var global = Windows.Globalization,
         format = Windows.Globalization.DateTimeFormatting,
         globalizationPreferences = Windows.System.UserProfile.GlobalizationPreferences;
 
-
+    // Get the current system calendar type
     var systemCalendarType = globalizationPreferences.calendars.getAt(0);
-    var cal = new global.Calendar(globalizationPreferences.languages, systemCalendarType, global.ClockIdentifiers.twelveHour);
 
+    // Construct a calendar that we can use for formatting dates
+    var calendar = new global.Calendar(globalizationPreferences.languages, systemCalendarType, global.ClockIdentifiers.twelveHour);
+
+    // Instatiate a throw-away instance of `DateTimeFormatter`
+    // just so we can acquire the geographic region in order 
+    // to construct a another `DateTimeFormatter` with all of 
+    // the appropriate values.
     var geographicRegion = new format.DateTimeFormatter("shortdate").resolvedGeographicRegion;
 
+    // Construct a `DateTimeFormatter` that will _only_ return the full month and full year for 
+    // the current system calendar settings.
     var dtf = new format.DateTimeFormatter(
         format.YearFormat.full,
         format.MonthFormat.full,
@@ -32,8 +46,8 @@
         format.SecondFormat.none,
         globalizationPreferences.languages,
         geographicRegion,
-        cal.getCalendarSystem(),
-        cal.getClock()
+        calendar.getCalendarSystem(),
+        calendar.getClock()
         );
 
     // Private Methods
