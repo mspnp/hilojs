f49cc8a95f8ca1b36a4da60cf92b7aba46bdc4d6
Derick Bailey <derickbailey@gmail.com>
2012-08-22 15:24:42 -0500

    an idea for a test for the rubber band view. not sure if this is a false positive, though

---
+++ Hilo.Specifications/specs/crop/rubberBandView.spec.js
@@ -0,0 +1,58 @@
+﻿describe("rubber band view", function () {
+
+    var canvasEl, rubberBandEl, rubberBand, rubberBandView;
+
+    function setupDOMElements() {
+        var frag = document.createDocumentFragment();
+        frag.appendChild(document.createElement("canvas"));
+
+        canvasEl = frag.querySelector("canvas");
+
+        rubberBandEl = document.createElement("div");
+        rubberBandEl.id = "rubberBand";
+        frag.appendChild(rubberBandEl);
+
+        var topLeft = document.createElement("div");
+        topLeft.id = "topLeft";
+        rubberBandEl.appendChild(topLeft);
+
+        var topRight = document.createElement("div");
+        topRight.id = "topRight";
+        rubberBandEl.appendChild(topRight);
+
+        var bottomLeft = document.createElement("div");
+        bottomLeft.id = "bottomLeft";
+        rubberBandEl.appendChild(bottomLeft);
+
+        var bottomRight = document.createElement("div");
+        bottomRight.id = "bottomRight";
+        rubberBandEl.appendChild(bottomRight);
+    }
+
+    beforeEach(function () {
+        setupDOMElements();
+        rubberBand = new Hilo.Crop.RubberBand(canvasEl);
+
+        rubberBandView = new Hilo.Crop.RubberBandView(rubberBand, canvasEl, rubberBandEl);
+    });
+
+    describe("when drawing the rubber band", function () {
+        var rubberBandCoords;
+
+        beforeEach(function () {
+            rubberBandCoords = rubberBand.getCoords();
+
+            rubberBandView.draw(rubberBandCoords);
+        });
+
+        it("should move the rubberBandEl to the rubberBand's coordinates", function () {
+            var bounds = rubberBandEl.getBoundingClientRect();
+
+            expect(bounds.top).equals(rubberBandCoords.startX);
+            expect(bounds.left).equals(rubberBandCoords.startY);
+            expect(bounds.width).equals(rubberBandCoords.endX - rubberBandCoords.startX);
+            expect(bounds.height).equals(rubberBandCoords.endY - rubberBandCoords.startY);
+        });
+    });
+
+});
