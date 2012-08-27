1d17482a3f6d9172a5ce764202f1274ba97f4d31
Derick Bailey <derickbailey@gmail.com>
2012-08-27 12:23:51 -0500

    basic specs and impl for crop menu presenter

---
+++ Hilo.Specifications/specs/crop/menuPresenter.spec.js
@@ -0,0 +1,72 @@
+﻿describe("crop menu presenter", function () {
+
+    var menu, el, crop, save, cancel;
+
+    beforeEach(function () {
+        crop = new Specs.WinControlStub();
+        save = new Specs.WinControlStub();
+        cancel = new Specs.WinControlStub();
+
+        el = new Specs.WinControlStub();
+        el.addQuerySelector("#crop", crop);
+        el.addQuerySelector("#save", save);
+        el.addQuerySelector("#cancel", cancel);
+
+        menu = new Hilo.Crop.MenuPresenter(el);
+    });
+
+    describe("when cropping", function () {
+        var handler;
+
+        beforeEach(function () {
+            handler = function () {
+                handler.wasCalled = true;
+            };
+
+            menu.addEventListener("crop", handler);
+            
+            crop.dispatchEvent("click");
+        });
+
+        it("should dispatch a crop event", function () {
+            expect(handler.wasCalled).equals(true);
+        });
+    });
+
+    describe("when saving", function () {
+        var handler;
+
+        beforeEach(function () {
+            handler = function () {
+                handler.wasCalled = true;
+            };
+
+            menu.addEventListener("save", handler);
+            
+            save.dispatchEvent("click");
+        });
+
+        it("should dispatch a save event", function () {
+            expect(handler.wasCalled).equals(true);
+        });
+    });
+
+    describe("when cancelling", function () {
+        var handler;
+
+        beforeEach(function () {
+            handler = function () {
+                handler.wasCalled = true;
+            };
+
+            menu.addEventListener("cancel", handler);
+            
+            cancel.dispatchEvent("click");
+        });
+
+        it("should dispatch a cancel event", function () {
+            expect(handler.wasCalled).equals(true);
+        });
+    });
+
+});
---
+++ Hilo/Hilo/crop/menuPresenter.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    "use strict";
+
+    // Menu Presenter Constructor
+    // --------------------------
+
+    function MenuPresenterConstructor(el) {
+        this.el = el;
+        this.setupButtons();
+    }
+
+    // Menu Presenter Members
+    // ----------------------
+
+    var menuPresenterMembers = {
+        setupButtons: function () {
+            this.addButtonHandler("#crop", this.cropClicked.bind(this));
+            this.addButtonHandler("#save", this.saveClicked.bind(this));
+            this.addButtonHandler("#cancel", this.cancelClicked.bind(this));
+        },
+
+        addButtonHandler: function(selector, handler){
+            var button = this.el.querySelector(selector);
+            button.addEventListener("click", handler);
+        },
+
+        cropClicked: function (e) {
+            e.preventDefault();
+            this.dispatchEvent("crop", {});
+        },
+
+        saveClicked: function (e) {
+            e.preventDefault();
+            this.dispatchEvent("save", {});
+        },
+
+        cancelClicked: function (e) {
+            e.preventDefault();
+            this.dispatchEvent("cancel", {});
+        }
+    };
+
+    // Menu Presenter Definition
+    // -------------------------
+
+    WinJS.Namespace.define("Hilo.Crop", {
+        MenuPresenter: WinJS.Class.mix(MenuPresenterConstructor, menuPresenterMembers, WinJS.Utilities.eventMixin)
+    });
+
+})();
