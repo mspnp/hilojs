92cb1f8096559ba1f072f78a5fd27a726973da8c
Derick Bailey <derickbailey@gmail.com>
2012-08-28 15:16:09 -0500

    initial extraction of cropconttroller from crop.js

--- Hilo/Hilo/crop/crop.js
+++ Hilo/Hilo/crop/crop.js
@@ -10,12 +10,6 @@
 (function () {
     "use strict";
 
-    // Imports And Constants
-    // ---------------------
-
-    var screenMaxHeight = 800;
-    var screenMaxWidth = 1600;
-
     // Page Control
     // ------------
 
@@ -37,115 +31,8 @@
             var rubberBandEl = document.querySelector("#rubberBand");
             var menuEl = document.querySelector("#appbar");
 
-            var storageFile, url, imageProps, imageRatio, imageScale, canvasSize;
-            var that = this,
-                cropOffset = { x: 0, y: 0 };
-
-            fileLoader.then(function (loadedImageArray) {
-
-                var picture = loadedImageArray[0];
-
-                storageFile = picture.storageFile;
-                url = URL.createObjectURL(storageFile);
-
-                return storageFile.properties.getImagePropertiesAsync();
-
-            }).then(function (props) {
-                imageProps = props;
-
-                imageRatio = that.getImageAspectRatio(props);
-                canvasSize = that.getCanvasSize(props, imageRatio);
-                imageScale = that.getImageScale(props, canvasSize);
-
-                that.sizeCanvas(canvasEl, canvasSize);
-
-                var rubberBand = new Hilo.Crop.RubberBand(canvasSize);
-                var pictureView = new Hilo.Crop.PictureView(context, rubberBand, url, canvasSize);
-                var rubberBandView = new Hilo.Crop.RubberBandView(rubberBand, canvasEl, rubberBandEl);
-                var rubberBandController = new Hilo.Crop.RubberBandController(rubberBand, canvasEl, rubberBandEl);
-
-                var menuPresenter = new Hilo.Crop.MenuPresenter(menuEl);
-                menuPresenter.addEventListener("crop", function () {
-
-                    var coords = rubberBand.getCoords();
-                    var scaledImageCoordinates = that.scaleCanvasCoordsToImage(imageScale, coords, cropOffset);
-                    var newRatio = that.getImageAspectRatio(scaledImageCoordinates);
-                    var canvasSize = that.getCanvasSize(scaledImageCoordinates, newRatio);
-
-                    that.sizeCanvas(canvasEl, canvasSize);
-
-                    rubberBand.reset(canvasSize);
-                    rubberBandController.reset();
-
-                    pictureView.reset(canvasSize, scaledImageCoordinates);
-                    rubberBandView.reset();
-
-                    //reset image scale and offset to match new canvas and image scaling
-                    imageScale = that.getImageScale(scaledImageCoordinates, canvasSize);
-                    cropOffset = { x: scaledImageCoordinates.startX, y: scaledImageCoordinates.startY };
-                });
-
-            });
-        },
-
-        // take the canvas coordinates and scale them to the real image coordinates
-        scaleCanvasCoordsToImage: function (imageScale, canvasCoords, cropOffset) {
-            var startX = canvasCoords.startX * imageScale.widthScale,
-                startY = canvasCoords.startY * imageScale.heightScale,
-                endX = canvasCoords.endX * imageScale.widthScale,
-                endY = canvasCoords.endY * imageScale.heightScale,
-                height = endY - startY,
-                width = endX - startX;
-
-            return {
-                startX: startX + cropOffset.x,
-                startY: startY + cropOffset.y,
-                endX: endX + cropOffset.x,
-                endY: endY + cropOffset.y,
-                height: height,
-                width: width
-            };
-        },
-
-        getImageAspectRatio: function (imageSize) {
-            return {
-                heightRatio: imageSize.width / imageSize.height,
-                widthRatio: imageSize.height / imageSize.width
-            }
-        },
-
-        getImageScale: function (imageSize, canvasSize) {
-            return {
-                heightScale: imageSize.height / canvasSize.height,
-                widthScale: imageSize.width / canvasSize.width
-            };
-        },
-
-        getCanvasSize: function (imageSize, aspectRatio) {
-            var height, width;
-
-            if (imageSize.height > imageSize.width) {
-                height = screenMaxHeight;
-                width = screenMaxHeight * aspectRatio.heightRatio;
-            } else {
-                width = screenMaxWidth;
-                height = screenMaxWidth * aspectRatio.widthRatio;
-                if (height > screenMaxHeight) {
-                    var scale = screenMaxHeight / height;
-                    height = height * scale;
-                    width = width * scale;
-                }
-            }
-
-            return {
-                height: height,
-                width: width
-            };
-        },
-
-        sizeCanvas: function (canvas, canvasSize) {
-            canvas.height = canvasSize.height;
-            canvas.width = canvasSize.width;
+            var cropController = new Hilo.Crop.CropController(fileLoader, canvasEl, context, rubberBandEl, menuEl);
+            cropController.start();
         },
 
     };
--- Hilo/Hilo/crop/cropController.js
+++ Hilo/Hilo/crop/cropController.js
@@ -3,35 +3,171 @@
 //  Hilo JS Guidance
 // ===============================================================================
 //  Copyright © Microsoft Corporation.  All rights reserved.
-//  This code released under the terms of the 
+//  that code released under the terms of the 
 //  Microsoft patterns & practices license (http://hilojs.codeplex.com/license)
 // ===============================================================================
 
 (function () {
 	"use strict";
 
+    // Imports And Constants
+    // ---------------------
+
+    var screenMaxHeight = 800;
+    var screenMaxWidth = 1600;
+
     // Constructor Function
     // --------------------
 
-	function CropControllerConstructor(canvasBounds, rubberBand, rubberBandView, pictureView) {
-	    var that = this;
-
-	    rubberBand.addEventListener("move", this.moveRubberBand.bind(this));
-    }
+	function CropControllerConstructor(fileLoader, canvasEl, context, rubberBandEl, menuEl) {
+	    this.fileLoader = fileLoader;
+	    this.canvasEl = canvasEl;
+	    this.context = context;
+	    this.rubberBandEl = rubberBandEl;
+	    this.menuEl = menuEl;
+	}
 
 	// Methods
 	// -------
 
 	var cropControllerMembers = {
-	    moveRubberBand: function (args) {
-	    }
+        start: function () {
+
+            var storageFile, url, imageProps, imageScale;
+            var that = this,
+                cropOffset = { x: 0, y: 0 };
+
+            this.fileLoader.then(function (loadedImageArray) {
+
+                var picture = loadedImageArray[0];
+
+                storageFile = picture.storageFile;
+                url = URL.createObjectURL(storageFile);
+
+                return storageFile.properties.getImagePropertiesAsync();
+
+            }).then(function (props) {
+                imageProps = props;
+
+                var imageRatio = that.getImageAspectRatio(props);
+                var canvasSize = that.getCanvasSize(props, imageRatio);
+                imageScale = that.getImageScale(props, canvasSize);
+
+                that.sizeCanvas(that.canvasEl, canvasSize);
+
+                var rubberBand = new Hilo.Crop.RubberBand(canvasSize);
+                var pictureView = new Hilo.Crop.PictureView(that.context, rubberBand, url, canvasSize);
+                var rubberBandView = new Hilo.Crop.RubberBandView(rubberBand, that.canvasEl, that.rubberBandEl);
+                var rubberBandController = new Hilo.Crop.RubberBandController(rubberBand, that.canvasEl, that.rubberBandEl);
+
+                var menuPresenter = new Hilo.Crop.MenuPresenter(that.menuEl);
+                menuPresenter.addEventListener("crop", function () {
+
+                    var coords = rubberBand.getCoords();
+                    var scaledImageCoordinates = that.scaleCanvasCoordsToImage(imageScale, coords, cropOffset);
+                    var imageRatio = that.getImageAspectRatio(scaledImageCoordinates);
+                    var canvasSize = that.getCanvasSize(scaledImageCoordinates, imageRatio);
+
+                    that.sizeCanvas(that.canvasEl, canvasSize);
+
+                    rubberBand.reset(canvasSize);
+                    rubberBandController.reset();
+
+                    pictureView.reset(canvasSize, scaledImageCoordinates);
+                    rubberBandView.reset();
+
+                    //reset image scale and offset to match new canvas and image scaling
+                    imageScale = that.getImageScale(scaledImageCoordinates, canvasSize);
+                    cropOffset = { x: scaledImageCoordinates.startX, y: scaledImageCoordinates.startY };
+                });
+
+            });
+        },
+
+        // take the canvas coordinates and scale them to the real image coordinates
+        scaleCanvasCoordsToImage: function (imageScale, canvasCoords, cropOffset) {
+            var startX = canvasCoords.startX * imageScale.widthScale,
+                startY = canvasCoords.startY * imageScale.heightScale,
+                endX = canvasCoords.endX * imageScale.widthScale,
+                endY = canvasCoords.endY * imageScale.heightScale,
+                height = endY - startY,
+                width = endX - startX;
+
+            return {
+                startX: startX + cropOffset.x,
+                startY: startY + cropOffset.y,
+                endX: endX + cropOffset.x,
+                endY: endY + cropOffset.y,
+                height: height,
+                width: width
+            };
+        },
+
+        getImageAspectRatio: function (imageSize) {
+            return {
+                heightRatio: imageSize.width / imageSize.height,
+                widthRatio: imageSize.height / imageSize.width
+            }
+        },
+
+        getImageScale: function (imageSize, canvasSize) {
+            return {
+                heightScale: imageSize.height / canvasSize.height,
+                widthScale: imageSize.width / canvasSize.width
+            };
+        },
+
+        getCanvasSize: function (imageSize, aspectRatio) {
+            var height, width, scale;
+
+            if (imageSize.height > imageSize.width) {
+
+                // height is greater than width, so shrink down to
+                // proper height, first
+                height = screenMaxHeight;
+                width = screenMaxHeight * aspectRatio.heightRatio;
+
+                // if the width of the image would go passed the max,
+                // then scale it down so that it fits the max
+                if (width > screenMaxWidth) {
+                    scale = screenMaxWidth / width;
+                    height = height * scale;
+                    width = width * scale;
+                }
+            } else {
+
+                // width is greater than height, so shrink down to
+                // the max width first.
+                width = screenMaxWidth;
+                height = screenMaxWidth * aspectRatio.widthRatio;
+
+                // if the height of the image would go passed the max,
+                // then scale it down so that it fits the max
+                if (height > screenMaxHeight) {
+                    scale = screenMaxHeight / height;
+                    height = height * scale;
+                    width = width * scale;
+                }
+            }
+
+            return {
+                height: height,
+                width: width
+            };
+        },
+
+        sizeCanvas: function (canvas, canvasSize) {
+            canvas.height = canvasSize.height;
+            canvas.width = canvasSize.width;
+        },
+
 	};
 
 	// Public API
 	// ----------
 
 	WinJS.Namespace.define("Hilo.Crop", {
-		CropController: WinJS.Class.mix(CropControllerConstructor, cropControllerMembers, WinJS.Utilities.eventMixin)
+	    CropController: WinJS.Class.mix(CropControllerConstructor, cropControllerMembers, WinJS.Utilities.eventMixin)
 	});
 
 })();
