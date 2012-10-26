825b95d41f406650993bb95aadddc3939b102ddd
Derick Bailey <derickbailey@gmail.com>
2012-10-26 15:11:03 -0500

    fixing crop preview so that clicking on the crop selection will crop the image, visually, and show only the area that was selected

--- Hilo/Hilo/crop/cropPresenter.js
+++ Hilo/Hilo/crop/cropPresenter.js
@@ -31,10 +31,6 @@
         //if (!storageFile || storageFile.name !== self.expectedFileName) {
         //    return self.navigation.navigate("/Hilo/hub/hub.html");
         //}
-
-        // We'll bind the methods ahead of time, merely to improve readability
-        this.beginCrop = this.beginCrop.bind(this);
-        this.handleAppBarEvents = this.handleAppBarEvents.bind(this);
     }
 
     // Methods
@@ -42,20 +38,8 @@
 
     var cropPresenterMembers = {
 
-        start: function () {
-            this.imageView.addEventListener("preview", this.cropImage.bind(this));
-            this.handleAppBarEvents();
-            this.beginCrop();
-        },
-
-        // Start the image cropping process by drawing the image and
-        // crop selection to scale, and then listen for the "crop" button click
-        beginCrop: function (props) {
-            this.offset = { x: 0, y: 0 };
-        },
-
         // register event listeners for all of the app bar buttons
-        handleAppBarEvents: function () {
+        start: function () {
             this.appBarPresenter.addEventListener("cancel", this.cancel.bind(this));
             this.appBarPresenter.addEventListener("save", this.saveImageAs.bind(this));
             this.appBarPresenter.addEventListener("unsnap", this.unSnapView.bind(this));
@@ -81,55 +65,7 @@
 
         cancel: function () {
             WinJS.Navigation.back();
-        },
-
-        // Run the image crop process, visually, to show what the crop result
-        // will look like when the file is saved.
-        // <SnippetHilojs_1609>
-        // <SnippetHilojs_1705>
-        cropImage: function () {
-            // Get the canvas-based rectangle of the crop selection
-            var coords = this.cropSelection.getCoords();
-
-            // calculate the selected area of the original image by scaling
-            // the canvas based selection out to the original image
-            var selectionRectScaledToImage = this.scaleCanvasRectToImage(this.imageToScreenScale, coords, this.offset);
-
-            // reset image scale so that it reflects the difference between
-            // the current canvas size (the crop selection), and the original 
-            // image size, then re-draw everything at that new scale
-            this.imageToScreenScale = this.calculateScaleToScreen(selectionRectScaledToImage);
-            this.drawImageSelectionToScale(selectionRectScaledToImage, this.imageToScreenScale);
-
-            // remember the starting location of the crop on the original image
-            // and not relative to the canvas size, so that cropping multiple times
-            // will correctly crop to what has been visually selected
-            this.offset = { x: selectionRectScaledToImage.startX, y: selectionRectScaledToImage.startY };
-        },
-        // </SnippetHilojs_1705>
-        // </SnippetHilojs_1609>
-
-        // take a rectangle that was based on a scaled canvas size
-        // and scale the rect up to the real image size, accounting
-        // for the offset of the rectangle location
-        scaleCanvasRectToImage: function (imageToScreenScale, canvasCoords, offset) {
-            var startX = canvasCoords.startX / imageToScreenScale,
-                startY = canvasCoords.startY / imageToScreenScale,
-                endX = canvasCoords.endX / imageToScreenScale,
-                endY = canvasCoords.endY / imageToScreenScale,
-                height = endY - startY,
-                width = endX - startX;
-
-            return {
-                startX: startX + offset.x,
-                startY: startY + offset.y,
-                endX: endX + offset.x,
-                endY: endY + offset.y,
-                height: height,
-                width: width
-            };
         }
-
     };
 
     // Crop Presenter Definition
--- Hilo/Hilo/crop/imageView.js
+++ Hilo/Hilo/crop/imageView.js
@@ -23,6 +23,8 @@
         this.context = canvasEl.getContext("2d");
         this.image = image;
 
+        this.offset = { x: 0, y: 0 };
+
         image.addEventListener("sizeUpdated", this.run.bind(this));
         image.addEventListener("urlUpdated", this.drawImage.bind(this));
 
@@ -38,11 +40,83 @@
     // --------------------
 
     var imageViewMembers = {
+        click: function () {
+            this.cropImage();
+        },
+
         run: function () {
             this.imageToScreenScale = this.calculateScaleToScreen(this.image.imageSize);
             var imageRect = this.sizeToRect(this.image.imageSize);
             this.drawImageSelectionToScale(imageRect, this.imageToScreenScale);
             this.drawImage();
+        },
+
+        drawImage: function () {
+            if (!this.image) { return; }
+
+            var imageHeight = this.imageSubset.endY - this.imageSubset.startY;
+            var imageWidth = this.imageSubset.endX - this.imageSubset.startX;
+
+            this.context.drawImage(
+                this.imageToPaint,
+
+                // cropped area of the image to draw
+                this.imageSubset.startX, this.imageSubset.startY, imageWidth, imageHeight,
+
+                // scale the cropped area in to the entire canvas
+                0, 0, this.canvasSize.width, this.canvasSize.height
+            );
+        },
+
+        reset: function (scaledImageCoordinates) {
+            this.canvasSize = this.canvasEl.getBoundingClientRect();
+            this.setImageSubset(scaledImageCoordinates);
+
+            // build a DOM image object that the canvas can paint
+            this.imageToPaint = new Image();
+            this.imageToPaint.onload = this.drawImage.bind(this);
+            this.imageToPaint.src = this.image.url;
+        },
+
+        cropImage: function(){
+            // Get the canvas-based rectangle of the crop selection
+            var coords = this.cropSelection.getCoords();
+
+            // calculate the selected area of the original image by scaling
+            // the canvas based selection out to the original image
+            var selectionRectScaledToImage = this.scaleCanvasRectToImage(this.imageToScreenScale, coords, this.offset);
+
+            // reset image scale so that it reflects the difference between
+            // the current canvas size (the crop selection), and the original 
+            // image size, then re-draw everything at that new scale
+            this.imageToScreenScale = this.calculateScaleToScreen(selectionRectScaledToImage);
+            this.drawImageSelectionToScale(selectionRectScaledToImage, this.imageToScreenScale);
+
+            // remember the starting location of the crop on the original image
+            // and not relative to the canvas size, so that cropping multiple times
+            // will correctly crop to what has been visually selected
+            this.offset = { x: selectionRectScaledToImage.startX, y: selectionRectScaledToImage.startY };
+        },
+
+        // take a rectangle that was based on a scaled canvas size
+        // and scale the rect up to the real image size, accounting
+        // for the offset of the rectangle location
+        scaleCanvasRectToImage: function (imageToScreenScale, canvasCoords, offset) {
+            var startX = canvasCoords.startX / imageToScreenScale,
+                startY = canvasCoords.startY / imageToScreenScale,
+                endX = canvasCoords.endX / imageToScreenScale,
+                endY = canvasCoords.endY / imageToScreenScale,
+                height = endY - startY,
+                width = endX - startX;
+
+            return {
+                startX: startX + offset.x,
+                startY: startY + offset.y,
+                endX: endX + offset.x,
+                endY: endY + offset.y,
+                height: height,
+                width: width
+            };
         },
 
         // convert a size (height/width) in to a rect
@@ -71,7 +145,7 @@
         // Calculate the canvas size, according to the scale, using
         // the crop selection rectangle
         drawImageSelectionToScale: function (cropRect, imageToScreenScale) {
-            var canvasSize = this.calculateSizeFromScale(this.image.imageSize, imageToScreenScale);
+            var canvasSize = this.calculateSizeFromScale(cropRect, imageToScreenScale);
             this.resizeCanvas(canvasSize);
 
             // reset and re-draw all of the controllers and presenters
@@ -99,42 +173,9 @@
             this.imgEl.src = imageUrl;
         },
 
-        drawImage: function () {
-            if (!this.image) { return; }
-
-            var imageHeight = this.imageSubset.endY - this.imageSubset.startY;
-            var imageWidth = this.imageSubset.endX - this.imageSubset.startX;
-
-            this.context.drawImage(
-                this.imageToPaint,
-
-                // cropped area of the image to draw
-                this.imageSubset.startX, this.imageSubset.startY, imageWidth, imageHeight,
-
-                // scale the cropped area in to the entire canvas
-                0, 0, this.canvasSize.width, this.canvasSize.height
-            );
-        },
-
         setImageSubset: function (imageCoords) {
             this.imageSubset = imageCoords;
         },
-
-        reset: function (scaledImageCoordinates) {
-            this.canvasSize = this.canvasEl.getBoundingClientRect();
-            this.setImageSubset(scaledImageCoordinates);
-
-            // build a DOM image object that the canvas can paint
-            this.imageToPaint = new Image();
-            this.imageToPaint.onload = this.drawImage.bind(this);
-            this.imageToPaint.src = this.image.url;
-        },
-
-        // <SnippetHilojs_1703>
-        click: function () {
-            this.dispatchEvent("preview");
-        },
-        // </SnippetHilojs_1703>
 
         // change the size of the specified canvas element to the calculated
         // size, and return the new size
